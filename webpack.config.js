e4aa1eec8614670f61422df24efb3f5c1a435b01
Pantuga <alexandrecarvalho06032009@gmail.com>
2025-03-18 15:57:16 +0000

    Directory rearrangement

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     entry: './compiled_js/main.js', // Entry point (your main compiled JS file)
     output: {
         filename: 'silly-lang.js', // Output file name
-        path: path.resolve(__dirname, 'dist'), // Output directory
+        path: path.resolve(__dirname, 'compiled_js/dist'), // Output directory
     },
     mode: 'production', // Use 'development' for debugging
     target: 'node', // Target Node.js environment
